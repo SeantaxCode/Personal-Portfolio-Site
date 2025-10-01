af0927a54fc2dfc339efa2fbaccd677694af4aed
Sean <chambs483@gmail.com>
2025-10-01 15:38:17 -0400

    UI changes, updated css

--- my-portfolio-site/src/App.tsx
+++ my-portfolio-site/src/App.tsx
@@ -1,8 +1,8 @@
-import ExploreNav from './ExploreNav.tsx'
-import SideNavBar from './SideNavBar.tsx'
-import TabNav from './TabNav.tsx'
-import CodeEditor from './CodeEditor.tsx'
-import Terminal from './Terminal.tsx'
+import ExploreNav from './Components/ExploreNav.tsx'
+import SideNavBar from './Components/SideNavBar.tsx'
+import TabNav from './Components/TabNav.tsx'
+import CodeEditor from './Components/CodeEditor.tsx'
+import Terminal from './Components/Terminal.tsx'
 
 function App() {
 
